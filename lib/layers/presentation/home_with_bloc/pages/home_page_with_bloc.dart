5fb8368042b0a2df0caa495fa64461ec57604327
guilherme <silva.guilherme09@gmail.com>
2021-01-20 16:40:51 -0300

    ref: Move MultiBlocProvider to home

--- lib/layers/presentation/home_with_bloc/pages/home_page_with_bloc.dart
+++ lib/layers/presentation/home_with_bloc/pages/home_page_with_bloc.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../injection_container.dart' as di;
 import '../bloc/home_bloc.dart';
 
 class HomePageWithBloc extends StatefulWidget {
@@ -14,37 +15,51 @@ class HomePageWithBloc extends StatefulWidget {
 class _HomePageWithBlocState extends State<HomePageWithBloc> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("RickAndMorty - Bloc"),
-      ),
-      body: Center(
-        child: Container(
-          color: Colors.white,
-          child: BlocConsumer<HomeCubit, HomeState>(
-            listener: (context, state) {},
-            builder: (context, state) {
-              if (state is Initial) {
-                return _showLoadButton();
-              } else if (state is Loading) {
-                return CircularProgressIndicator();
-              } else if (state is Error) {
-                return Container();
-              } else if (state is Success) {
-                return _showCharactersList(state);
-              } else {
-                return Container();
-              }
-            },
-          ),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<HomeCubit>(
+          create: (BuildContext context) => di.sl<HomeCubit>(),
         ),
+      ],
+      // provider a context with HomeCubit down the tree
+      child: Builder(
+        builder: (BuildContext context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("RickAndMorty - Bloc"),
+            ),
+            body: Center(
+              child: Container(
+                color: Colors.white,
+                child: BlocConsumer<HomeCubit, HomeState>(
+                  listener: (context, state) {},
+                  builder: (context, state) {
+                    if (state is Initial) {
+                      return _showLoadButton(context);
+                    } else if (state is Loading) {
+                      return CircularProgressIndicator();
+                    } else if (state is Error) {
+                      return Container();
+                    } else if (state is Success) {
+                      return _showCharactersList(state);
+                    } else {
+                      return Container();
+                    }
+                  },
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
 
-  Widget _showLoadButton() {
+  Widget _showLoadButton(BuildContext context) {
     return MaterialButton(
-      onPressed: _loadData,
+      onPressed: () {
+        _loadData(context);
+      },
       child: Text("Load data"),
       color: Colors.blue,
     );
@@ -67,8 +82,8 @@ class _HomePageWithBlocState extends State<HomePageWithBloc> {
     );
   }
 
-  void _loadData() {
-    final cubit = context.bloc<HomeCubit>();
+  void _loadData(BuildContext context) {
+    final cubit = context.read<HomeCubit>();
     cubit.showLoadingAndFetchCharacters();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_state_notifier/flutter_state_notifier.dart';
 import 'package:flutter_stetho/flutter_stetho.dart';
 import 'package:provider/provider.dart';
 
 import 'injection_container.dart' as di;
 import 'layers/domain/usecases/get_all_characters.dart';
-import 'layers/presentation/home_with_bloc/bloc/home_bloc.dart';
 import 'layers/presentation/home_with_provider/notifiers/home_notifier.dart';
 import 'layers/presentation/home_with_state_notifier/state/home_state.dart'
     as stateNotifier;
@@ -39,24 +37,16 @@ class MyApp extends StatelessWidget {
               HomeStateNotifier(getAllCharacters: di.sl<GetAllCharacters>()),
         ),
       ],
-      // * USED by BLOC version
-      child: MultiBlocProvider(
-        providers: [
-          BlocProvider<HomeCubit>(
-            create: (BuildContext context) => di.sl<HomeCubit>(),
-          ),
-        ],
-        child: MaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-            // This makes the visual density adapt to the platform that you run
-            // the app on. For desktop platforms, the controls will be smaller and
-            // closer together (more dense) than on mobile platforms.
-            visualDensity: VisualDensity.adaptivePlatformDensity,
-          ),
-          home: MainPage(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          // This makes the visual density adapt to the platform that you run
+          // the app on. For desktop platforms, the controls will be smaller and
+          // closer together (more dense) than on mobile platforms.
+          visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
+        home: MainPage(),
       ),
     );
   }
