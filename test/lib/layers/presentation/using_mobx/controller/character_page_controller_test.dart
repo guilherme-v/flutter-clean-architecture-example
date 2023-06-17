e57f42dea8c1c0b408c7093f9b81f5a4f8aad9a1
guilherme silva <silva.guilherme09@gmail.com>
2023-06-17 11:37:49 -0300

    Add MobX tests

---
+++ test/lib/layers/presentation/using_mobx/controller/character_page_controller_test.dart
@@ -0,0 +1,54 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:rickmorty/layers/presentation/using_mobx/controller/character_page_controller.dart';
+
+import '../../../../../fixtures/fixtures.dart';
+import '../../helper/pump_app.dart';
+
+void main() {
+  late CharacterPageController controller;
+  late GetAllCharactersMock getAllCharactersMock;
+
+  setUp(() {
+    getAllCharactersMock = GetAllCharactersMock();
+    controller = CharacterPageController(getAllCharacters: getAllCharactersMock);
+  });
+
+  test('fetchNextPage success', () async {
+    // Mock the response of GetAllCharacters
+    final mockCharacterList = [...characterList1, ...characterList2];
+    when(() => getAllCharactersMock.call(page: any(named: 'page')))
+        .thenAnswer((_) async => mockCharacterList);
+
+    expect(controller.contentStatus, equals(Status.initial));
+
+    // Call the method under test
+    await controller.fetchNextPage();
+
+    // Verify the interactions and expected values
+    expect(controller.contentStatus, equals(Status.success));
+    expect(controller.currentPage, equals(2));
+    expect(controller.charactersList, equals(mockCharacterList));
+    expect(controller.hasReachedEnd, isFalse);
+  });
+
+  test('fetchNextPage has reached end', () async {
+    // Mock an empty response from GetAllCharacters
+    when(() => getAllCharactersMock(page: any(named: 'page')))
+        .thenAnswer((_) async => []);
+
+    // Set hasReachedEnd to true to simulate the end of the list
+    // controller.hasReachedEnd = true;
+
+    expect(controller.contentStatus, equals(Status.initial));
+
+    // Call the method under test
+    await controller.fetchNextPage();
+
+    // Verify the interactions and expected values
+    expect(controller.contentStatus, equals(Status.success));
+    expect(controller.currentPage, equals(2));
+    expect(controller.charactersList, isEmpty);
+    expect(controller.hasReachedEnd, isTrue);
+  });
+}
