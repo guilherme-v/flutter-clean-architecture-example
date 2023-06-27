20ae9b2faedba790943638ee7ef16c0d81f65d53
guilherme silva <silva.guilherme09@gmail.com>
2023-06-26 22:24:39 -0300

    Update Assets and CharacterCard

---
+++ lib/layers/presentation/assets.dart
@@ -0,0 +1,41 @@
+import 'dart:ui';
+
+class Asset {
+  // ---------------------------------------------------------------------------
+  // Images
+  // ---------------------------------------------------------------------------
+  static const String _img = 'assets/images';
+
+  static const String bgBase = '$_img/bg-base.jpg';
+  static const String bgLightReceive = '$_img/bg-light-receive.png';
+  static const String mgBaseBottom = '$_img/mg-base-bottom.png';
+  static const String mgLightReceiveBottom =
+      '$_img/mg-light-receive-bottom.png';
+  static const String mgLightEmitBottom = '$_img/mg-light-emit-bottom.png';
+  static const String fgBase = '$_img/fg-base.png';
+  static const String fgLightReceive = '$_img/fg-light-receive.png';
+  static const String fgEmit = '$_img/fg-emit.png';
+
+  static const String cardBorderBottomLeft = '$_img/border-bottom-left.png';
+  static const String cardBorderTopRight = '$_img/border-top-right.png';
+  static const String cardBorderTopLeft = '$_img/border-top-left.png';
+  static const String cardBorderBottomRight = '$_img/border-bottom-right.png';
+
+  // ---------------------------------------------------------------------------
+  // Shaders
+  // ---------------------------------------------------------------------------
+  static const String _shaders = 'assets/shaders';
+  static const String orbShader = '$_shaders/orb_shader.frag';
+  static const String uiShader = '$_shaders/ui_glitch.frag';
+}
+
+typedef Shaders = ({FragmentShader orb, FragmentShader ui});
+
+Future<Shaders> loadShaders() async => (
+      orb: (await _loadShader(Asset.orbShader)),
+      ui: (await _loadShader(Asset.uiShader)),
+    );
+
+Future<FragmentShader> _loadShader(String path) async {
+  return (await FragmentProgram.fromAsset(path)).fragmentShader();
+}
