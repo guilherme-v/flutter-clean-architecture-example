fa1ed4445846bcbc748f8b22a5af799b1fff029c
guilherme silva <silva.guilherme09@gmail.com>
2023-08-26 23:57:37 -0300

    details: Init CharacterDetailsPage

---
+++ lib/layers/presentation/using_bloc/details_page/character_details_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+// -----------------------------------------------------------------------------
+// Page
+// -----------------------------------------------------------------------------
+class CharacterDetailsPage extends StatelessWidget {
+  const CharacterDetailsPage({super.key});
+
+  static MaterialPageRoute route() {
+    return MaterialPageRoute(builder: (context) {
+      return const CharacterDetailsPage();
+    },);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+// -----------------------------------------------------------------------------
+// Screen
+// -----------------------------------------------------------------------------
+class _Screen extends StatelessWidget {
+  const _Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
+// -----------------------------------------------------------------------------
+// Content
+// -----------------------------------------------------------------------------
+class _Content extends StatelessWidget {
+  const _Content({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
