bd5da19f5acf41c792fb8781f5d92d38bc8a5ffd
guilherme silva <silva.guilherme09@gmail.com>
2023-10-11 21:49:12 -0300

    bloc: Remove unused param from details page

--- lib/layers/presentation/using_bloc/details_page/view/character_details_page.dart
+++ lib/layers/presentation/using_bloc/details_page/view/character_details_page.dart
@@ -8,16 +8,14 @@ import 'package:rickmorty/layers/presentation/using_bloc/details_page/bloc/chara
 // Page
 // -----------------------------------------------------------------------------
 class CharacterDetailsPage extends StatelessWidget {
-  const CharacterDetailsPage({required this.character, super.key});
-
-  final Character character;
+  const CharacterDetailsPage({super.key});
 
   static Route<void> route({required Character character}) {
     return MaterialPageRoute(
       builder: (context) {
         return BlocProvider(
           create: (_) => CharacterDetailsBloc(character: character),
-          child: CharacterDetailsPage(character: character),
+          child: const CharacterDetailsPage(),
         );
       },
     );
@@ -58,126 +56,130 @@ class _Content extends StatelessWidget {
     final textTheme = Theme.of(context).textTheme;
     final colorScheme = Theme.of(context).colorScheme;
 
-    final character = context.select(
-      (CharacterDetailsBloc b) => b.state.character,
-    );
-
     return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          CachedNetworkImage(
-            imageUrl: character.image!,
-            fit: BoxFit.cover,
-            height: 300,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    character.name ?? '',
-                    style: textTheme.displaySmall!.copyWith(
-                      color: colorScheme.onSurfaceVariant,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    'Status: ${character.isAlive ? 'ALIVE!' : 'DEAD!!'}',
-                    style: textTheme.titleMedium!.copyWith(
-                      color: character.isAlive
-                          ? Colors.lightGreen
-                          : Colors.redAccent,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  const Divider(height: 1),
-                  const SizedBox(height: 16),
-                  Text(
-                    'Origin: ${character.origin?.name ?? ''}',
-                    style: textTheme.bodyMedium!.copyWith(
-                      color: colorScheme.onSurfaceVariant,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    'Last location: ${character.location?.name ?? ''}',
-                    style: textTheme.bodyMedium!.copyWith(
-                      color: colorScheme.onSurfaceVariant,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    'Species: ${character.species ?? ''}',
-                    style: textTheme.bodyMedium!.copyWith(
-                      color: colorScheme.onSurfaceVariant,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    'Type: ${character.type ?? '?'}',
-                    style: textTheme.bodyMedium!.copyWith(
-                      color: colorScheme.onSurfaceVariant,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    'Gender: ${character.gender ?? ''}',
-                    style: textTheme.bodyMedium!.copyWith(
-                      color: colorScheme.onSurfaceVariant,
-                    ),
-                  ),
-                  // const SizedBox(height: 4),
-                ],
+      child: Builder(
+        builder: (ctx) {
+          final character = ctx.select(
+            (CharacterDetailsBloc b) => b.state.character,
+          );
+
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              CachedNetworkImage(
+                imageUrl: character.image!,
+                fit: BoxFit.cover,
+                height: 300,
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 16.0),
-            child: Text(
-              'Episodes:',
-              style: textTheme.bodyLarge!.copyWith(
-                color: colorScheme.onSurfaceVariant,
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 80,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: character.episode?.length ?? 0,
-              itemBuilder: (context, index) {
-                final ep = character.episode![index];
-                final name = ep.split('/').last;
-                return Padding(
-                  padding: const EdgeInsets.only(left: 12.0, top: 8),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(16),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text(
+                        character.name ?? '',
+                        style: textTheme.displaySmall!.copyWith(
+                          color: colorScheme.onSurfaceVariant,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                      color: colorScheme.surfaceVariant,
-                    ),
-                    height: 80,
-                    width: 80,
-                    child: Center(
-                      child: Text(
-                        name,
-                        style: textTheme.bodyLarge!.copyWith(
+                      const SizedBox(height: 8),
+                      Text(
+                        'Status: ${character.isAlive ? 'ALIVE!' : 'DEAD!!'}',
+                        style: textTheme.titleMedium!.copyWith(
+                          color: character.isAlive
+                              ? Colors.lightGreen
+                              : Colors.redAccent,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      const Divider(height: 1),
+                      const SizedBox(height: 16),
+                      Text(
+                        'Origin: ${character.origin?.name ?? ''}',
+                        style: textTheme.bodyMedium!.copyWith(
                           color: colorScheme.onSurfaceVariant,
                         ),
                       ),
-                    ),
+                      const SizedBox(height: 8),
+                      Text(
+                        'Last location: ${character.location?.name ?? ''}',
+                        style: textTheme.bodyMedium!.copyWith(
+                          color: colorScheme.onSurfaceVariant,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        'Species: ${character.species ?? ''}',
+                        style: textTheme.bodyMedium!.copyWith(
+                          color: colorScheme.onSurfaceVariant,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        'Type: ${character.type ?? '?'}',
+                        style: textTheme.bodyMedium!.copyWith(
+                          color: colorScheme.onSurfaceVariant,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        'Gender: ${character.gender ?? ''}',
+                        style: textTheme.bodyMedium!.copyWith(
+                          color: colorScheme.onSurfaceVariant,
+                        ),
+                      ),
+                      // const SizedBox(height: 4),
+                    ],
                   ),
-                );
-              },
-            ),
-          )
-        ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 16.0),
+                child: Text(
+                  'Episodes:',
+                  style: textTheme.bodyLarge!.copyWith(
+                    color: colorScheme.onSurfaceVariant,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 80,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: character.episode?.length ?? 0,
+                  itemBuilder: (context, index) {
+                    final ep = character.episode![index];
+                    final name = ep.split('/').last;
+                    return Padding(
+                      padding: const EdgeInsets.only(left: 12.0, top: 8),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(16),
+                          ),
+                          color: colorScheme.surfaceVariant,
+                        ),
+                        height: 80,
+                        width: 80,
+                        child: Center(
+                          child: Text(
+                            name,
+                            style: textTheme.bodyLarge!.copyWith(
+                              color: colorScheme.onSurfaceVariant,
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              )
+            ],
+          );
+        },
       ),
     );
   }
