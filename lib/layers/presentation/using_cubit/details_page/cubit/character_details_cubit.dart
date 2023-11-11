6cef999f810ea9897c156dafc96b4384ccffdee7
guilherme silva <silva.guilherme09@gmail.com>
2023-11-11 08:47:03 -0300

    cubit: Prepare character_details to be tested

--- lib/layers/presentation/using_cubit/details_page/cubit/character_details_cubit.dart
+++ lib/layers/presentation/using_cubit/details_page/cubit/character_details_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 import 'package:rickmorty/layers/domain/entity/character.dart';
 
 part 'character_details_state.dart';
--- lib/layers/presentation/using_cubit/details_page/cubit/character_details_state.dart
+++ lib/layers/presentation/using_cubit/details_page/cubit/character_details_state.dart
@@ -1,7 +1,10 @@
 part of 'character_details_cubit.dart';
 
-class CharacterDetailsState {
+class CharacterDetailsState with EquatableMixin {
   const CharacterDetailsState(this.character);
 
   final Character character;
+
+  @override
+  List<Object?> get props => [character];
 }
